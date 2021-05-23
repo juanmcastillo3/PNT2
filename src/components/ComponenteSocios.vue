d18c1d8be733c257039d46367e02d401e0ce0b53
Gaucho <juanmcastillo3@gmail.com>
2021-05-23 19:10:36 -0300

    Modificacion de ABM socios

--- src/components/ComponenteSocios.vue
+++ src/components/ComponenteSocios.vue
@@ -8,13 +8,14 @@
       <input type="text" v-model="dni">
       <button @click="agregarSocio">Agregar</button>
     </form>
-    <li v-for="(socio,index) in socios" :key="index">
+    <li v-for="(socio,index) in socios" v-bind:key="index">
       {{socio.name}}
       {{socio.surname}}
       {{socio.phone}}
       {{socio.document}}
     </li>
-
+    <button @click="eliminarSocio(item)">Eliminar</button>
+    <button @click="modificarSocio(element)">Modificar</button>
   </div>
 
 </template>
@@ -43,7 +44,17 @@ export default {
       document: this.dni,
     })
     console.log(this.socios);
-    }
+    },
+    eliminarSocio: function(item){
+    this.socios.splice(item,1)
+    },
+  /*   modificarSocio: function(element){
+      let position = this.socios.map(element => this.dni.indexOf(this.dni))
+      this.socios[position].name = this.Nombre
+      this.socios[position].surname = this.Apellido
+      this.socios[position].phone = this.Telefono
+      this.socios[position].document = this.dni
+    }, */
   },
     }
 
